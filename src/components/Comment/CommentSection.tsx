06a3699724f5c33f46a4022933e610de63c99783
Kroro1208 <yamagata_7580@yahoo.co.jp>
2025-07-13 14:07:23 +0900

    修正

--- src/components/Comment/CommentSection.tsx
+++ src/components/Comment/CommentSection.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import CommentItem from "./CommentItem.tsx";
 import {
--- src/components/Tag/TagPopularityDashboard.tsx
+++ src/components/Tag/TagPopularityDashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import { useTagStats, TagStats } from "../../hooks/useTagStats.ts";
 import {
   getTopTagsByPopularity,
